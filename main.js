fe1dbc03151ac0f419f880ba41c972e3705f3034
diego <diegozaratean@gmail.com>
2024-08-17 01:43:13 +0000

    finish radom caard

--- main.js
+++ main.js
@@ -46,3 +46,37 @@ function tirarDados(){
 
 
 
+// DONE: PSEUDOCODIGO
+// DONE:agregar input ancho y alto en html
+// DONE: agregar el boton 'redimensionar' al html
+// DONE: cuando le doy click al boton redimensionar ejecutar una funcion
+//  DONE: crear una funcion redimensionar o cambiar tamaño dados
+function redimensionar(){
+  console.log('redimensionar')
+  //  DONE:leer el input ancho 
+  let ancho = document.getElementById('ancho').value
+  console.log('ancho',ancho)
+  //  DONE: leer el input alto 
+  let alto = document.getElementById('alto').value
+  console.log('alto',alto)
+  // DONE: poder cambiar el tamaño del dado desde js
+  document.getElementById('dado1').style.height = `${alto}px`
+  document.getElementById('dado1').style.width = ancho + 'px'
+  document.getElementById('dado2').style.height = `${alto}px`
+  document.getElementById('dado2').style.width = ancho + 'px'
+}
+
+
+// document.getElementById('addevent').addEventListener("click", function (e) {
+//   console.log('le diste click a addevent'); // logs the className of my_element
+// });
+
+document.getElementById('addevent').onclick = ()=>{
+  console.log('le diste click a addevent'); // logs the className of my_element
+}
+
+
+setInterval(tirarDados, 3000);
+
+
+
